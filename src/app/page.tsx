da5f0618374a4a82d1cf3856932c4ad7c76f380e
Tomasz Stanisz <fremen1990@gmail.com>
2023-12-01 18:42:35 +0100

    :pencil2: Create routes structure

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1 className="h1-bold">Devthomas Overflopw START!!</h1>
-            <h2 className="h2-bold">Devthomas Overflopw START!!</h2>
-            <h3 className="h3-bold">Devthomas Overflopw START!!</h3>
-        </div>)
-}
