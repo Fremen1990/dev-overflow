b30da658e3d0c0de3c8d7717b6aaf4367d19f806
Tomasz Stanisz <fremen1990@gmail.com>
2023-12-04 19:58:33 +0100

    :art: Add Global ThemeProvider

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import {createContext, ReactNode, useContext, useEffect, useState} from "react";
+
+enum Theme {
+    LIGHT = "light",
+    DARK = "dark"
+}
+
+const defaultTheme = {
+    theme: Theme.DARK,
+    handleToggleTheme: () => {
+    }
+}
+
+const ThemeContext = createContext(defaultTheme)
+
+export function ThemeProvider({children}: { children: ReactNode }) {
+
+    const [theme, setTheme] = useState<Theme>(Theme.DARK)
+
+    const handleToggleTheme = () => {
+        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        setTheme(newTheme);
+        document.documentElement.classList.remove(theme);
+        document.documentElement.classList.add(newTheme);
+    }
+
+    useEffect(() => {
+        handleToggleTheme()
+    }, [theme])
+
+    return (
+        <ThemeContext.Provider value={
+            {theme, handleToggleTheme}
+        }>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export function useTheme() {
+    const context = useContext(ThemeContext)
+
+    if (context === undefined) {
+        throw new Error("useTheme must be used within a ThemeProvider")
+    }
+
+    return context
+}
